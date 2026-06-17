ec8bd37c620e290652eda9d0aa35c7a98632a51c
valsalama <vsalama@uade.edu.ar>
2026-06-17 10:32:02 -0300

    prueba interfaz visual con javaswing

---
+++ vistas/VentanaIngreso.java
@@ -0,0 +1,10 @@
+package vistas;
+import javax.swing.JFrame;
+
+public class VentanaIngreso extends JFrame{
+    public VentanaIngreso() {
+    setTitle("Ingreso sistema");
+    setSize(300, 200);
+    setVisible(true);
+    }
+}
