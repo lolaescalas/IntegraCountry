a2ee85baee6c45a08d98417298ade676bdd8e470
lucialorenzo <lucialorenzoo19@gmail.com>
2026-06-18 16:44:45 -0300

    Clases de usuario, barrio y lote

--- modelo/espacios/Barrio.java
+++ modelo/espacios/Barrio.java
@@ -3,5 +3,20 @@ package modelo.espacios;
 public class Barrio {
 
     private String nombre;
-    
+    private String direccion;
+    private List<Lote> lotes = new ArrayList<>();
+
+    public Barrio(String nombre, String direccion) {
+        this.nombre = nombre;
+        this.direccion = direccion;
+    }
+
+    public void agregarLote(Lote lote) {
+        lotes.add(lote);
+    }
+
+    public String getNombre() {return nombre; }
+    public String getDireccion() { return direccion: }
+    public List<Lote> getLotes() {return lotes;}
+
 }
--- modelo/espacios/Reserva.java
+++ modelo/espacios/Reserva.java
@@ -2,4 +2,29 @@ package modelo.espacios;
 
 public class Reserva {
 
+    private String fecha;
+    private String horario;
+    private String estado;
+    private EspacioComun espacio;
+
+
+    public Reserva(String fecha, String horario, EspacioComun espacio) {
+        this.fecha = fecha;
+        this.horario = horario;
+        this.espacio = espacio;
+        this.estado = "ACTIVA";       
+    }
+
+    public void cancelar() {
+        this.estado = "CANCELADA";
+        this.espacio.setDisponible(true);
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public EspacioComun getEspacio() {return espacio; }
+
+
 }
