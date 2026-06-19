379a7b4afc8fd90cdd1c2b89100e6a58d5f32e11
lucialorenzo <lucialorenzoo19@gmail.com>
2026-06-19 19:34:52 -0300

    solicitud guarda lista, agregar/eliminar, notificar() que recorre la lista y avisa con msj que el estado cambio

--- modelo/abstractas/Solicitud.java
+++ modelo/abstractas/Solicitud.java
@@ -1,12 +1,45 @@
 package modelo.abstractas;
 
-import modelo.usuarios.Empleado;
-import patrones.state.IEstadoSolicitud;
+import java.util.ArrayList;
+import java.util.List;
 
-public abstract class Solicitud {
+import modelo.usuarios.Empleado;
+import patrones.observer.IObservable;
+import patrones.observer.IObservador;
+import patrones.state.IEstadoSolicitud;
+import patrones.state.EstadoPendiente;
+import servicio.AsignadorEmpleado;
+
+public abstract class Solicitud implements IObservable {
     
     private IEstadoSolicitud estado;
     private Empleado empleado;
+    private AsignadorEmpleado asignador;
+    private List<IObservable> observadores = new ArrayList<>();
+
+
+    public Solicitud(AsignadorEmpleado asignador) {
+        this.asignador = asignador; 
+        this.estado = new EstadoPendiente();
+    }
+
+    @Override
+    public void agregar(IObservador o) {
+        observadores.add(o);
+    }
+
+    @Override
+    public void eliminar(IObservador o) {
+        observadores.remove(o);
+    }
+
+    @Override
+    public void notificar() {
+        String mensaje = "La solicitud cambio al estado: " + estado.obtenerNombre();
+        for (IObservador o : observadores) {
+            o.actualizar(mensaje);
+        }
+    }
 
     public IEstadoSolicitud getEstado() {
         return estado;
@@ -14,6 +47,7 @@ public abstract class Solicitud {
 
     public void setEstado(IEstadoSolicitud estado) {
         this.estado = estado;
+        notificar();
     }
 
     public Empleado getEmpleado() {
@@ -22,5 +56,9 @@ public abstract class Solicitud {
 
     public void setEmpleado(Empleado empleado) {
         this.empleado = empleado;
+    }
+
+    public AsignadorEmpleado getAsignador() {
+        return asignador;
     }
 }
