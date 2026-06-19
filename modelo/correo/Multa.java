414553f585cdf04b41daf8bac6483be1eb204659
lucialorenzo <lucialorenzoo19@gmail.com>
2026-06-19 14:44:35 -0300

    agrego multa

--- modelo/correo/Multa.java
+++ modelo/correo/Multa.java
@@ -2,4 +2,14 @@ package modelo.correo;
 
 public class Multa {
 
+    private double monto;
+    private String motivo;
+
+    public Multa(double monto, String motivo) {
+        this.monto = monto;
+        this.motivo = motivo;
+    }
+
+    public double getMonto() { return monto; }
+    public String getMotivo() { return motivo; }
 }
