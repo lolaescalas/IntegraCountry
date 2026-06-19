27903d256ad3b4361746b5e9f37fda9d8569a28f
lucialorenzo <lucialorenzoo19@gmail.com>
2026-06-19 20:16:20 -0300

    sacar static

--- repositorio/RepositorioEmpleados.java
+++ repositorio/RepositorioEmpleados.java
@@ -6,7 +6,7 @@ import modelo.usuarios.Empleado;
 
 public class RepositorioEmpleados {
 
-    private static List<Empleado> empleados = List.of(
+    private List<Empleado> empleados = List.of(
     new Empleado(1, "Juan Montero", "30111222", "juan.montero@barrio.com", "1130011222"),
     new Empleado(2, "Maria Lopez", "28999888", "maria.lopez@barrio.com", "1139998888"),
     new Empleado(3, "Pedro Alcorta", "31222333", "pedro.alcorta@barrio.com", "1131222333"),
