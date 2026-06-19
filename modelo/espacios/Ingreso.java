c907b62891a30c58d9c99ee18633055c6590c588
lucialorenzo <lucialorenzoo19@gmail.com>
2026-06-19 20:02:56 -0300

    clase ingreso y AdminitracionFacade

---
+++ modelo/espacios/Ingreso.java
@@ -0,0 +1,28 @@
+package modelo.espacios;
+
+import java.time.LocalDate;
+
+import modelo.abstractas.Usuario;
+
+public class Ingreso {
+
+    private LocalDate fecha;
+    private String hora;
+    private String tipo;
+    private Usuario persona;
+    private String destino;
+
+    public Ingreso(String hora, String tipo, Usuario persona, String destino) {
+        this.fecha = LocalDate.now();
+        this.hora = hora;
+        this.tipo = tipo;
+        this.persona = persona;
+        this.destino = destino;
+    }
+
+    public LocalDate getFecha() { return fecha; }
+    public String getHora() { return hora; }
+    public String getTipo() { return tipo; }
+    public Usuario getPersona() { return persona; }
+    public String getDestino() { return destino; }
+}
---
+++ patrones/facade/AdministracionFacade.java
@@ -0,0 +1,43 @@
+package fachada;
+
+import enums.Prioridad;
+import modelo.abstractas.Solicitud;
+import modelo.abstractas.Usuario;
+import modelo.espacios.Barrio;
+import modelo.espacios.Ingreso;
+import patrones.factory.SolicitudFactory;
+import patrones.strategy.CanalInterno;
+import patrones.strategy.Notificacion;
+
+public class AdministracionFacade {
+
+    private SolicitudFactory solicitudFactory;
+    private Barrio barrio;
+
+    public AdministracionFacade(Barrio barrio) {
+        this.barrio = barrio;
+        this.solicitudFactory = new SolicitudFactory();
+    }
+
+    public Solicitud gestionarReclamo(Prioridad prioridad) {
+        Solicitud reclamo = solicitudFactory.crearReclamo(prioridad);
+
+        Notificacion notificacion = new Notificacion(new CanalInterno());
+        reclamo.agregar(notificacion);
+
+        reclamo.getEstado().manejarSolicitud(reclamo);
+
+        return reclamo;
+    }
+
+    public Ingreso registrarIngreso(String hora, String tipo, Usuario persona, String destino) {
+        Ingreso ingreso = new Ingreso(hora, tipo, persona, destino);
+        System.out.println("[ACCESO] " + tipo + " registrado: "
+                + persona.getNombre() + " hacia " + destino + " a las " + hora);
+        return ingreso;
+    }
+
+    public void registrarResidente(Usuario residente) {
+        System.out.println("[REGISTRO] Residente registrado: " + residente.getNombre());
+    }
+}
